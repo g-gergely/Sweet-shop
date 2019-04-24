755a6b20b691893d491efbf1041ad41ee8d9c403
Geiszlinger Gergely <ggergely@gmail.com>
2019-04-24 15:00:11 +0200

    Server side of filtering data

---
+++ src/main/java/com/codecool/shop/controller/ProductFilterByCategory.java
@@ -0,0 +1,51 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ProductFilterByCategory {
+    @WebServlet(urlPatterns = {"/category"})
+    public class ProductController extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            ProductDao productDataStore = ProductDaoMem.getInstance();
+            ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+
+            List<Product> productsByCategory = new ArrayList<>();
+
+
+            int productCategoryId= Integer.parseInt(req.getParameter("category_id"));
+            ProductCategory productCategory = productCategoryDataStore.find(productCategoryId);
+            productsByCategory= productDataStore.getBy(productCategory);
+
+            TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+            WebContext context = new WebContext(req, resp, req.getServletContext());
+
+            //Rendering back the result
+            context.setVariable("productsByCategory", productsByCategory);
+            engine.process("product/index.html", context, resp.getWriter());
+
+
+        }
+    }
+}
