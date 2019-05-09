26fc88ad2c5e110844815062ed61a24c9d1bd9de
Geiszlinger Gergely <ggergely@gmail.com>
2019-05-09 16:47:04 +0200

    Setup database in SQL

---
+++ src/main/sql/loading_db.sql
@@ -0,0 +1,66 @@
+INSERT INTO suppliers
+VALUES ("Granny's", "Traditional, home-made biscuits");
+INSERT INTO suppliers
+VALUES ("Cake Wonders", "A wide variety of delicious cakes.");
+INSERT INTO suppliers
+VALUES ("Sweeter Sweets", "Candies - not only for children!");
+INSERT INTO suppliers
+VALUES ("Choco", "All kinds of fine dark and milk chocolate.");
+INSERT INTO suppliers
+VALUES ("Sweet Desire", "Special sweets for special occasions.");
+
+
+INSERT INTO categories
+VALUES ("Biscuit", "Pastry", "Various types of sweet baked cakes.");
+INSERT INTO categories
+VALUES ("Cake", "Pastry", "Various types of sweet baked cakes.");
+INSERT INTO categories
+VALUES ("Cupcake", "Pastry", "Various types of sweet baked cakes.");
+INSERT INTO categories
+VALUES ("Candy", "Sweets", "Sweet, sugary food (chocolate or candies).");
+INSERT INTO categories
+VALUES ("Chocolate", "Sweets", "Sweet, sugary food (chocolate or candies).");
+
+
+INSERT INTO products
+VALUES ("Crispy cookies", 9, "USD", "If something is as crispy as an apple, it must be healthy too!", 1, 1);
+INSERT INTO products
+VALUES ("Macaroons", 7, "USD", "Light - not just in weight but calories as well.", 1, 1);
+INSERT INTO products
+VALUES ("Pancake with honey", 18, "USD", "Soft as a pillow, with the treasure of bees.", 1, 1);
+INSERT INTO products
+VALUES ("Biscuit Collection", 22, "USD", "Finest of our wares, best for tea in the afternoons.", 1, 1);
+INSERT INTO products
+VALUES ("Cheesecake", 30, "USD", "Cheesy dreams!", 2, 2);
+INSERT INTO products
+VALUES ("Chocolate cake", 32, "USD", "Dark as night but sweet as our children's smile.", 2, 2);
+INSERT INTO products
+VALUES ("Candy-pops", 3, "USD", "Candies on a pole, available in various flavours!", 4, 5);
+INSERT INTO products
+VALUES ("Fruit candies", 2, "USD", " They have it in their name: FRUIT!", 4, 3);
+INSERT INTO products
+VALUES ("Rubber bear fruit gums", 2, "USD", "So cute that you would like to die for it.", 4, 3);
+INSERT INTO products
+VALUES ("Gummy worms", 2, "USD", "They'll live longer than Grey Worm.", 4, 3);
+INSERT INTO products
+VALUES ("Chocolate selection", 15, "USD", "You will get a little from everything... but you will want more.", 5, 5);
+INSERT INTO products
+VALUES ("Heart-shape chocolate selection", 17, "USD", "Almost as effective as oyster...", 5,5);
+INSERT INTO products
+VALUES ("Dark chocolate with coffee", 10, "USD", "Coffee? You said COFFEE?", 5, 4);
+INSERT INTO products
+VALUES ("Milk chocolate with hazelnut", 8, "USD", "It is soo good that it will have you wrapped in haze.", 5, 4);
+INSERT INTO products
+VALUES ("Dark chocolate with pistachio", 9, "USD", "Chocolate with melted pistachio.", 5, 4);
+INSERT INTO products
+VALUES ("Milk chocolate with raspberry", 7, "USD", "Julius Cesar's favourite!", 5, 4);
+INSERT INTO products
+VALUES ("Blueberry cupcake", 12, "USD", "We didn't make it in a microwave!", 3, 5);
+INSERT INTO products
+VALUES ("Chocolate cupcake", 11, "USD", "Only with the best Swiss chocolate!", 3, 5);
+INSERT INTO products
+VALUES ("Vanilla cupcake", 10, "USD", "The dream of every princess!", 3, 5);
+INSERT INTO products
+VALUES ("Fantastic Five", 35, "USD", "Some only dream of cake - we bake it happen!", 2, 2);
+
+
